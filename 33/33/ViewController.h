dc865b9fa4bf33248482b6c84e9ecd36568625d4
jwjw <51435@qq.com>
2015-08-14 10:03:47 +0800

    添加工程

---
+++ 33/33/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  33
+//
+//  Created by JW on 15/8/14.
+//  Copyright (c) 2015年 jiawen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 33/33Tests/_3Tests.m
@@ -0,0 +1,40 @@
+//
+//  _3Tests.m
+//  33Tests
+//
+//  Created by JW on 15/8/14.
+//  Copyright (c) 2015年 jiawen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _3Tests : XCTestCase
+
+@end
+
+@implementation _3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
